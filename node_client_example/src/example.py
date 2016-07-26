ab91e6aef5d7d6ae78d3bc83000cf81c3b71f01d
Michael Dockter <dockter-84@users.noreply.github.com>
2016-07-26 13:43:53 -0400

    Update example.py

--- node_client_example/src/example.py
+++ node_client_example/src/example.py
@@ -1,7 +1,9 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 import pyspark
-sc = pyspark.SparkContext()
+
+if not 'sc' in globals():
+    sc = pyspark.SparkContext()
 rdd = sc.parallelize(range(1000))
 sample = rdd.takeSample(False, 5)
 print(sample)
